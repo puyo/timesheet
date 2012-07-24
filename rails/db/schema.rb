d7185bfec1cb7fbb9914a682ec4b49c16be8c090
Gregory McIntyre <blue.puyo@gmail.com>
2012-07-24 17:44:46 +1000

    Remove users.

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -12,12 +12,4 @@
 # It's strongly recommended to check this file into your version control system.
 
 ActiveRecord::Schema.define(:version => 20120522010724) do
-
-  create_table "users", :force => true do |t|
-    t.string   "basecamp_uid"
-    t.string   "name"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
-  end
-
 end
