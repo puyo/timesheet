39cfeb6a2282ae3143fafbdf4a3c75067bebdcdd
Gregory McIntyre <blue.puyo@gmail.com>
2012-07-26 16:45:06 +1000

    Remove ActiveRecord.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-ActiveResource::Base.logger = ActiveRecord::Base.logger
-
 # Initialize the rails application
 Timesheet::Application.initialize!
