7701fcecbe6b28e61abde2d6c8f03cb21c8bd600
Gregory McIntyre <blue.puyo@gmail.com>
2012-07-27 16:00:35 +1000

    Factor out more basecamp API request logic.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,16 +25,54 @@ class ApplicationController < ActionController::Base
     redirect_to edit_basecamp_key_url
   end
 
-  def basecamp(method, path, args = {})
-    result = Typhoeus::Request.send(method, [basecamp_host, path].join, typhoeus_args.merge(args))
-    if method.to_s == 'get' and result.code == 401
-      raise BasecampError, result.body
-    end
-    result
+  def basecamp_get_json(path, args = {})
+    JSON.parse(basecamp(:get, path, args).body)
   end
 
+  def basecamp_get_xml(path, args = {})
+    XmlSimple.xml_in(basecamp(:get, path, args).body)
+  end
+
+  def basecamp_post(path, args = {})
+    basecamp(:post, path, args)
+  end
+
+  def basecamp_put(path, args = {})
+    basecamp(:put, path, args)
+  end
+
+  def basecamp_delete(path, args = {})
+    basecamp(:delete, path, args)
+  end
+
+  def basecamp_get_json_async(path, args = {}, &block)
+    req = basecamp(:new, path, args)
+    req.on_complete do |result|
+      if result.success?
+        block.call JSON.parse(result.body)
+      else
+        raise BasecampError, result.body
+      end
+    end
+    req
+  end
+
+  def basecamp_get_xml_async(path, args = {}, &block)
+    req = basecamp(:new, path, args)
+    req.on_complete do |result|
+      if result.success?
+        block.call XmlSimple.xml_in(result.body)
+      else
+        raise BasecampError, result.body
+      end
+    end
+    req
+  end
+
+  private
+
   def basecamp_host
-    "https://protein-one.basecamphq.com"
+    'https://protein-one.basecamphq.com'
   end
 
   def typhoeus_args
@@ -51,5 +89,16 @@ class ApplicationController < ActionController::Base
       :username => session[:basecamp_api_token],
       :password => 'X',
     }
+  end
+
+  def basecamp(method, path, args = {})
+    obj = Typhoeus::Request.send(method, [basecamp_host, path].join, typhoeus_args.merge(args))
+    if obj.is_a?(Typhoeus::Request)
+      obj
+    elsif obj.code == 200
+      obj
+    else
+      raise BasecampError, obj.body
+    end
   end
 end
--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -3,23 +3,23 @@ class TimeEntriesController < ApplicationController
     @from = params[:from] || 1.week.ago.to_date.to_s(:slug)
     @to = params[:to] || Date.today.to_s(:slug)
     fetch_data_for_index
+    new_time_entry_for_form
   end
 
   def create
     todo_list_items = []
-    lists = JSON.parse(basecamp(:get, "/projects/#{project_id}/todo_lists.json", :verbose => true).body)
+    lists = basecamp_get_json("/projects/#{project_id}/todo_lists.json")
     lists['records'].each do |x|
       list_id = x['id']
-      items = JSON.parse(basecamp(:get, "/todo_lists/#{list_id}/todo_items.json", :verbose => true).body)
+      items = basecamp_get_json("/todo_lists/#{list_id}/todo_items.json")
       todo_list_items += items['records']
     end
     item = todo_list_items.select{|t| t['content'] == params[:time_entry][:job_code] }.first
     if item
-      new_response = basecamp(:post, "/todo_items/#{item['id']}/time_entries.xml", :verbose => true, :body => xml)
+      new_response = basecamp_post("/todo_items/#{item['id']}/time_entries.xml", :body => xml)
     else
       @message = 'No job code found on that project'
       return
-      #new_response = basecamp(:post, "/projects/#{project_id}/time_entries.xml", :verbose => true, :body => xml)
     end
     if id = new_response.headers_hash['Location'][/\d+$/]
       @time_entry = load_time_entry(id)
@@ -28,15 +28,14 @@ class TimeEntriesController < ApplicationController
   end
 
   def update
-    @response = basecamp(:put, "/time_entries/#{params[:id]}.xml", :verbose => true, :body => xml)
-    id = params[:id] if @response.code == 200
-    @time_entry = load_time_entry(id)
+    result = basecamp_put("/time_entries/#{params[:id]}.xml", :body => xml)
+    @time_entry = load_time_entry(params[:id])
     @time_entry.project_id = project_id
   end
 
   def destroy
-    @response = basecamp(:delete, "/time_entries/#{params[:id]}.xml", :verbose => true)
-    @id = params[:id] if @response.code == 200
+    result = basecamp_delete("/time_entries/#{params[:id]}.xml")
+    @id = params[:id]
   end
 
   private
@@ -58,58 +57,45 @@ class TimeEntriesController < ApplicationController
   end
 
   def fetch_data_for_index
-    @me = JSON.parse(basecamp(:get, '/me.json').body)
-    projects_request = basecamp(:new, '/projects.json')
-    people_request = basecamp(:new, "/companies/#{@me['firmId']}/people.json")
-    time_entries_request = basecamp(:new, '/time_entries/report.xml', :params => {
+    @me = basecamp_get_json('/me.json')
+    projects_request = basecamp_get_json_async('/projects.json') do |json|
+      @projects = json['records']
+    end
+    people_request = basecamp_get_json_async("/companies/#{@me['firmId']}/people.json") do |json|
+      @people = json['records']
+    end
+    time_entries_request = basecamp_get_xml_async('/time_entries/report.xml', :params => {
       :subject_id => @me['id'],
       :from => @from.gsub('-', ''),
       :to => @to.gsub('-', ''),
-    })
-
+    }) do |xml|
+      # TODO: NoMethodError (undefined method `map' for nil:NilClass):
+      @time_entries = xml['time-entry'].map{|el| time_entry_from_xml(el) }
+    end
     hydra = Typhoeus::Hydra.new
     hydra.queue projects_request
     hydra.queue people_request
     hydra.queue time_entries_request 
     hydra.run
-
-    projects_data = JSON.parse(projects_request.handled_response.body)
-    people_data = JSON.parse(people_request.handled_response.body)
-    time_entries_data = XmlSimple.xml_in(time_entries_request.handled_response.body)
-
-    @projects = projects_data['records']
-    @people = people_data['records']
-    @time_entries = time_entries_data['time-entry'].map do |xml_data|
-      time_entry_from_xml(xml_data)
-    end
-
     @projects_index = {}
     @projects.each do |project|
       @projects_index[project['id'].to_i] = project
     end
-
     @people_index = {}
     @people.each do |person|
       @people_index[person['id'].to_i] = person
     end
+    @time_entries = @time_entries.group_by(&:date)
+  end
 
-    @time_entries.each do |entry|
-      entry.project_name = @projects_index[entry.project_id]
-    end
-
-    @time_entries = @time_entries.group_by(&:date).sort_by{|date,entries| date}
+  def new_time_entry_for_form
     @time_entry = TimeEntry.new
     @time_entry.person_id = @me['id']
   end
 
   def load_time_entry(id)
-    r = basecamp(:get, "/time_entries/#{id}.xml")
-    if r.code == 200
-      data = XmlSimple.xml_in(r.body)
-      time_entry_from_xml(data).tap{|result| result.id = id }
-    else
-      nil
-    end
+    data = basecamp_get_xml("/time_entries/#{id}.xml")
+    time_entry_from_xml(data).tap{|result| result.id = id }
   end
 
   def time_entry_from_xml(xml_data)
