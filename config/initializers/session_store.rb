33e9b6f6efba3f3f926ceadf5f9350bc46be2db8
Gregory McIntyre <blue.puyo@gmail.com>
2012-08-01 13:04:28 +1000

    This probably won't fix the cookie problems...

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Timesheet::Application.config.session_store :cookie_store, :key => '_timesheet_session'
+Timesheet::Application.config.session_store :cookie_store, :key => '_timesheet_session', :domain => '.protein'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
