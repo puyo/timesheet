c8ce5afe114442d0795a289590df178bdd4630e2
Gregory McIntyre <blue.puyo@gmail.com>
2012-08-02 10:02:50 +1000

    Bump up session timeout to 1 year.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Timesheet::Application.config.session_store :cookie_store, :key => '_timesheet_session', :expire_after => 60.minutes
+Timesheet::Application.config.session_store :cookie_store, :key => '_timesheet_session', :expire_after => 1.year
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
